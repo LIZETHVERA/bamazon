786cac3237e7f9f8ddc70737df80014d42329615
LIZETHVERA <lizeth.veraro@gmail.com>
2019-05-16 21:38:46 -0700

    insert the second menu

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var empty = require('is-empty');
-var Table = require('cli-table');
+var Table = require('cli-table3');
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -38,30 +38,63 @@ function start() {
       "exit"
     ]
   })
-  .then(function(answer) {
-    switch (answer.action) {
-      case "See the list of products":
-        displayProducts();
-        break;
+    .then(function (answer) {
+      switch (answer.action) {
+        case "See the list of products":
+          displayProducts();
+          secondMenu()
+          break;
 
         case "Buy a product by ID":
-        runUserBuy();
-        break;
+          runUserBuy();
+          break;
 
         case "exit":
-        connection.end();
-        break;
-    }
-  });
+          connection.end();
+          break;
+      }
+    });
+}
+
+function secondMenu(){
+  inquirer.prompt({
+    name: "action",
+    type: "confirm",
+    message: "Would you like to buy a product?",
+}).then(function(answer) {
+  // console.log(answer);
+  
+  if (answer.action == true){
+    runUserBuy();
+  } else {
+    connection.end();
+  }
+});
+
 }
 
 function displayProducts() {
-  connection.query("SELECT * FROM products", function(err, res) {
-    for (var i = 0; i < res.length; i++) {
-      console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price );
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    // Cli-Table display code with Color
+    var table = new Table(
+      {
+        head: ["ID", "Product Name", "Department Name", "Quantity","Price"],
+        colWidths: [5, 40, 20, 12,12],
+      });
+
+    // Set/Style table headings and Loop through entire inventory
+    for (var i = 0; i < res.length - 1; i++) {
+      table.push(
+        [res[i].item_id, res[i].product_name, res[i].department_name, res[i].stock_quantity, res[i].price]
+      );
     }
-    console.log("-----------------------------------");
+    console.log(table.toString());
+
+
   });
+
+
 }
 
 
@@ -77,7 +110,7 @@ function runUserBuy() {
           if (isNaN(value) === false && !value == "") {
             return true;
           }
-          console.log("Please introduce a number");
+          console.log("Please enter a number");
           return false;
         }
       },
@@ -87,7 +120,7 @@ function runUserBuy() {
         message: "How many units of the product would you like to buy?: ",
         validate: function (value) {
           if (isNaN(value) === false && !value == "") {
-             return true;
+            return true;
 
           }
           console.log("Please introduce a number");
@@ -109,8 +142,8 @@ function runUserBuy() {
           console.log(stockAvaible);
           console.log("The product you have selected is: " + res[i].product_name + ". with the ID number: " + res[i].item_id);
 
-
-          if (stockAvaible > 0) {
+          // Case escenario 1: units less or equal to our inventory
+          if (answer.units <= stockAvaible) {
 
             var updateStock = connection.query(
               "UPDATE products SET ? WHERE ?",
@@ -125,17 +158,20 @@ function runUserBuy() {
               function (err, res) {
                 console.log(res.affectedRows + " Your order was processed Thank you for buying with us!\n");
 
-                // Call deleteProduct AFTER the UPDATE completes
-
+               
               }
             );
             console.log("The unit price is: " + res[i].price);
             console.log("The total cost of your purchase is: " + (res[i].price * answer.units));
+          
+            // Case escenario 2: We have some units available but not the whole order. 
+          } else if (stockAvaible > 0 && answer.units > stockAvaible) {
 
-
-            // console.log("No se que es" + updateStock.sql);
-          } else {
-            console.log("Sorry, Insufficient quantity!")
+            console.log("Sorry, Insufficient quantity! We have: " + stockAvaible + " Units of the product available");
+          }
+          else {
+            // Case escenario 2: We dont have anything. 
+            console.log("Sorry, Insufficient quantity!");
           }
           // console.log((res[i].stock_quantity) - answer.units);
         }
